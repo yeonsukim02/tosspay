2d8376ad030281d353f47196a5d202b623c215e2
yeonsukim02 <gkfn3470@gmail.com>
2024-09-26 14:38:24 +0900

    Add files via upload

--- script/brand.js
+++ script/brand.js
@@ -1,4 +1,4 @@
-const b_name_wrap= new Swiper(".b_name_wrap",{
+const brand_name_wrap= new Swiper(".brand_name_wrap",{
     slidesPerView: 12,
     spaceBetween: 40,
     autoplay:{delay:2000},
@@ -10,12 +10,14 @@ const best_product_wrap= new Swiper(".best_product_wrap",{
     autoplay:{delay:3000},
     loop:true,
     navigation: {
-        nextEl: '.swiper-button-next', // 다음 슬라이드로 이동하는 버튼
-        prevEl: '.swiper-button-prev', // 이전 슬라이드로 이동하는 버튼
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
     },
     pagination: {
-		el: ".swiper-pagination",   //페이징 태그 클래스 설정 
+		el: ".swiper-pagination",
 		type: 'progressbar',
+        innerWidth: '100px',
+        outerWidth: '1400px',
     },
 })
 const pick_product_wrap= new Swiper(".pick_product_wrap",{
